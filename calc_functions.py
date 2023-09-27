ca724673cefb58a18cfb9d2550902918589e904d
Djulizah <djul.b74123@gmail.com>
2023-09-27 09:54:02 +0800

    Initial commit

---
+++ calc_functions.py
@@ -0,0 +1,15 @@
+# add(a, b): Menerima dua argumen bilangan bulat dan mengembalikan hasil penjumlahan mereka
+def add(a,b):
+    return a+b
+
+# subtract(a, b): Menerima dua argumen bilangan bulat dan mengembalikan hasil pengurangan a dikurangkan dari b.
+def subtract(a,b):
+    return a-b
+
+# multiply(a, b): Menerima dua argumen bilangan bulat dan mengembalikan hasil perkalian a dikalikan dengan b.
+def multiply(a,b):
+    return a*b
+
+# div(a,b): Menerima dua argumen bilangan bulat dan mengembalikan hasil pembagian bulan a dibagi dengan b.
+def div(a,b):
+    return a/b
---
+++ calc_test.py
@@ -0,0 +1,35 @@
+import pytest
+from calc_functions import *
+
+# Gunakan pytest untuk menulis pengujian unit untuk fungsi-fungsi yang telah Anda buat. 
+'''
+Buat pengujian yang mencakup skenario berikut:
+• Pengujian untuk setiap fungsi dengan argumen positif.
+• Pengujian untuk setiap fungsi dengan argumen negatif.
+• Pengujian untuk setiap fungsi dengan argumen nol.
+• Pengujian untuk kasus khusus, seperti hasil penjumlahan nol, hasil perkalian nol, dan lain-lain.
+• Pastikan pengujian menguji kasus yang beragam dan memeriksa apakah fungsi-fungsi Anda menghasilkan output yang benar
+'''
+'''
+Buatlah sebuah fixture yang menghasilkan data input yang berbeda untuk pengujian Anda. 
+Anda bisa menggunakan parameterisasi fixture untuk menguji berbagai kombinasi input.
+• Terapkan marker pada pengujian Anda untuk mengkategorikan mereka. Misalnya, Anda 
+dapat menggunakan marker ”tambahkurang" untuk pengujian yang berkaitan dengan
+operasi penjumlahan dan pengurangan. Dan marker “kalibagi” untuk operasi perkalian dan 
+pembagian. Anda juga dapat menambahkan deskripsi singkat pada marker tersebut. 
+• Pelaporan: Buat sebuah dokumen atau laporan yang mencakup hasil pengujian Anda, 
+termasuk hasil dari pytest. Jelaskan pengujian apa yang telah Anda lakukan dan apa yang 
+Anda pelajari dari hasilnya
+'''
+
+@pytest.fixture(params=[(2,3),(0,0),(-1,1),(-1,-1),(100,-100)])
+def input_data(request):
+    return request.param
+
+@pytest.mark.ujitambah
+def test_add(input_data):
+    x,y = input_data
+    result = add(x,y)
+    assert result > 0
+
+
